33885da9deef16a0d3c9fdd63e9739c068a4b366
bdreiss <bd_reiss@yahoo.de>
2023-03-16 08:42:05 +0100

    Created first tests and dummy test methods for DataAPI

--- DataAPI/Test/main/java/com/bdreiss/dataAPI/DataModelTest.java
+++ DataAPI/Test/main/java/com/bdreiss/dataAPI/DataModelTest.java
@@ -2,16 +2,93 @@ package main.java.com.bdreiss.dataAPI;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DataModelTest {
 
+	private final String SAVE_FILE_PATH = "./";
+	
+	DataModel data;
+	
+	@BeforeEach
+	void prepare() {
+		data = new DataModel(SAVE_FILE_PATH);
+	}
+
+	//tests whether the constructor creates a new file
 	@Test
-	void test() {
-		DataModel data = new DataModel(".");
-		data.load();
-//		data.print();
-		fail("Not yet implemented");
+	void dataModel() {
+		//assert file exists
+		assert(new File(SAVE_FILE_PATH + data.getSaveFileName()).exists());		
+	}
+
+	//tests whether method deletes file correctly
+	@Test
+	void deleteSaveFile() {
+		data.deleteSaveFile();
+		//assert file does not exist
+		assert(!new File(SAVE_FILE_PATH + data.getSaveFileName()).exists());
+	}
+
+	@AfterEach
+	void cleanUp() {
+		data.deleteSaveFile();
+	}
+	
+	@Test
+	void addMigraine() {
+		
+	}
+
+	@Test
+	void addHabit() {
+		
+	}
+	
+	@Test
+	void addSymptom() {
+		
+	}
+	
+	@Test
+	void addRemedy() {
+		
+	}
+	
+	@Test
+	void returnMigraines() {
+		
+	}
+	
+	@Test
+	void returnHabits() {
+		
+	}
+	
+	@Test
+	void returnSymptoms() {
+		
+	}
+	
+	@Test
+	void returnRemedies() {
+		
+	}
+
+	@Test
+	void save() {
+		
+	}
+	
+	@Test
+	void load() {
+		
 	}
 
 }
