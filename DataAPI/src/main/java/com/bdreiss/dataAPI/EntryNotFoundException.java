155cf19e4a3c0afcf0bb2bb9daec61b23c95fb44
bdreiss <bd_reiss@yahoo.de>
2023-04-10 19:06:33 +0200

    DataAPI: removing last entry also removes key in map

---
+++ DataAPI/src/main/java/com/bdreiss/dataAPI/EntryNotFoundException.java
@@ -0,0 +1,9 @@
+package main.java.com.bdreiss.dataAPI;
+
+public class EntryNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	//TODO implement EntryNotFoundException
+	
+}
