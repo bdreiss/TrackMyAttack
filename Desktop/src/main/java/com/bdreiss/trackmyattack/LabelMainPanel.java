b04fa3808c2a60d612c349feb75d0a97b3545fe5
bdreiss <bd_reiss@yahoo.de>
2023-08-29 08:26:57 +0200

    Desktop: added comments

--- Desktop/src/main/java/com/bdreiss/trackmyattack/LabelMainPanel.java
+++ Desktop/src/main/java/com/bdreiss/trackmyattack/LabelMainPanel.java
@@ -13,7 +13,7 @@ import com.bdreiss.dataAPI.SymptomDataModel;
 import main.java.com.bdreiss.trackmyattack.GeoData.GeoData;
 
 /*
- * Panel 
+ * Panel that holds all labels, consisting of CAUSES, SYMPTOMS, REMEDIES and GEO_DATA 
  */
 
 public class LabelMainPanel extends JPanel {
@@ -22,6 +22,7 @@ public class LabelMainPanel extends JPanel {
 
 	public LabelMainPanel(DataModel data, GeoData geoData) {
 
+		//filler Panel for spacing groups
 		class FillPanel extends JPanel {
 			private static final long serialVersionUID = 1L;
 
@@ -34,6 +35,7 @@ public class LabelMainPanel extends JPanel {
 			}
 		}
 
+		//height modifiers for filler Panels
 		double[] modifiers = { 0.25, 1, 1, 1, 1 };
 
 		setLayout(new GridBagLayout());
@@ -43,41 +45,32 @@ public class LabelMainPanel extends JPanel {
 		c.gridx = 0;
 		c.gridy = 0;
 
+		//add filler and CAUSES
 		add(new FillPanel(modifiers[0]), c);
-
 		c.gridy++;
-
 		LabelPanel causeLabels = new LabelPanel(new CauseDataModel(data));
-
 		add(causeLabels, c);
-
 		c.gridy++;
+		
+		//add filler and SYMPTOMS
 		add(new FillPanel(modifiers[1]), c);
-
 		c.gridy++;
-
 		LabelPanel symptomLabels = new LabelPanel(new SymptomDataModel(data));
-
 		add(symptomLabels, c);
-
 		c.gridy++;
+		
+		//add filler and REMEDIES
 		add(new FillPanel(modifiers[2]), c);
-
 		c.gridy++;
-
 		LabelPanel remedyLabels = new LabelPanel(new RemedyDataModel(data));
-
 		add(remedyLabels, c);
-
 		c.gridy++;
+		
+		//add filler, GEO_DATA and final filler
 		add(new FillPanel(modifiers[3]), c);
-
 		c.gridy++;
-
 		LabelPanel geoLabels = new LabelPanel(geoData);
-
 		add(geoLabels, c);
-
 		c.gridy++;
 		add(new FillPanel(modifiers[4]), c);
 
