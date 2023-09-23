01b17f7c23f6648d1b6385f88f56d2582efa49be
bdreiss <bd_reiss@yahoo.de>
2023-09-23 09:00:52 +0200

    Desktop: Added documentation, comments and TODOs

--- Desktop/src/main/java/com/bdreiss/trackmyattack/GeoData/APIQuery.java
+++ Desktop/src/main/java/com/bdreiss/trackmyattack/GeoData/APIQuery.java
@@ -20,7 +20,7 @@ public interface APIQuery {
 	 * @param endDate last date in range for which to look up data
 	 * @param coordinates Point2D.Double representing coordinates for which meteorological data should be queried
 	 * @param data DataModel in which data should be added 
-	 * @param category Category that is used for storing 
+	 * @param category Category that is used for storing data
 	 */
 	void query(LocalDate startDate, LocalDate endDate, Point2D.Double coordinates, DataModel data, Category category);
 
--- Desktop/src/main/java/com/bdreiss/trackmyattack/GeoSphereAustria/APIQueryGeoSphereAustria.java
+++ Desktop/src/main/java/com/bdreiss/trackmyattack/GeoSphereAustria/APIQueryGeoSphereAustria.java
@@ -14,7 +14,6 @@ import javax.net.ssl.HttpsURLConnection;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import com.bdreiss.dataAPI.AbstractDataModel;
 import com.bdreiss.dataAPI.DataModel;
 import com.bdreiss.dataAPI.enums.Category;
 
@@ -22,49 +21,44 @@ import main.java.com.bdreiss.trackmyattack.GeoData.APIQuery;
 import main.java.com.bdreiss.trackmyattack.GeoData.GeoDataType;
 import main.java.com.bdreiss.trackmyattack.GeoData.GeoDatum;
 
+/**
+ * Class implementing the APIQuery interface, containing methods for retrieving
+ * meteorological data from Austrian stations via the API of GeoSphere Austria.
+ */
+
 public class APIQueryGeoSphereAustria implements APIQuery {
 
 	private static final String PREFIX_AUSTRIA = "https://dataset.api.hub.geosphere.at/v1/station/historical/klima-v1-1d?parameters=t,tmax,tmin,druckmit,rel,dampfmit&start=";
 	private static final String INFIX1_AUSTRIA = "T00:00&end=";
 	private static final String INFIX2_AUSTRIA = "T00:01&station_ids=";
 
+	// Main function for testing purposes
 	public static void main(String args[]) {
 		APIQueryGeoSphereAustria apiQuery = new APIQueryGeoSphereAustria();
-		
-		apiQuery.getNearestStationAustria(new Point2D.Double(48.15980285249154, 16.34069433192093));
+		apiQuery.query(LocalDate.now().minusDays(4), LocalDate.now().minusDays(1),
+				new Point2D.Double(48.15980285249154, 16.34069433192093), new DataModel(null), Category.SYMPTOM);
 	}
-	
-	
+
 	@Override
-	public void query(LocalDate startDate, LocalDate endDate, Point2D.Double coordinates, DataModel data, Category category) {
-		parseJSON(JSONQuery(startDate, endDate, coordinates), data, category);
+	public void query(LocalDate startDate, LocalDate endDate, Point2D.Double coordinates, DataModel data,
+			Category category) {
+
+		String jsonString = JSONQuery(startDate, endDate, coordinates);
+		parseJSON(jsonString, data, category);
 	}
 
-	//extract data from the JSONObject retrieved from https://dataset.api.hub.zamg.ac.at 
-	public ArrayList<Float> extractData(JSONObject jso) {
-		ArrayList<Float> data = new ArrayList<Float>();
-
-		String dataString = jso.get("data").toString();
-
-		dataString = dataString.substring(1, dataString.length() - 1);
-
-		for (String s : dataString.split(","))
-			if (s.equals("null"))
-				data.add(null);
-			else			
-				data.add(Float.parseFloat(s));
-			
-		return data;
-	}
-
+	// parse JSON and add meteorological data to DataModel
 	public void parseJSON(String jsonString, DataModel data, Category category) {
+		
 		JSONObject jso = new JSONObject(jsonString);
 
 		ArrayList<LocalDate> timestamps = new ArrayList<LocalDate>();
 
+		//get JSON string for timestamps and remove first and last character '[' and ']'
 		String timestampsString = jso.get("timestamps").toString();
 		timestampsString = timestampsString.substring(1, timestampsString.length() - 1);
 
+		//split string, get single dates and add them to timestamps
 		for (String s : timestampsString.split(",")) {
 
 			String dateString = s.substring(1, s.length() - 1).split("T")[0];
@@ -75,6 +69,8 @@ public class APIQueryGeoSphereAustria implements APIQuery {
 			LocalDate ld = LocalDate.now().withYear(year).withMonth(month).withDayOfMonth(day);
 			timestamps.add(ld);
 		}
+
+		//get JSON strings for Druck, Temp etc. and extractData resulting in ArrayList<Float>
 		JSONArray jsa = (JSONArray) jso.get("features");
 		JSONObject jso2 = (JSONObject) jsa.get(0);
 		JSONObject jso3 = (JSONObject) jso2.get("properties");
@@ -94,6 +90,8 @@ public class APIQueryGeoSphereAustria implements APIQuery {
 		ArrayList<Float> relData = extractData(jsoRel);
 		ArrayList<Float> dampfData = extractData(jsoDampf);
 
+		
+		//for every timestamp add data from ArrayLists extracted above
 		for (int i = 0; i < timestamps.size(); i++) {
 
 			LocalDate date = timestamps.get(i);
@@ -112,48 +110,64 @@ public class APIQueryGeoSphereAustria implements APIQuery {
 			GeoDataType humidityE = GeoDataType.HUMIDITY;
 
 			if (temperatureMedian != null)
-				data.addDatumDirectly(category, temperatureMedianE.toString(),
-						new GeoDatum(date.atStartOfDay(), temperatureMedian, temperatureMedianE.lowerBound(), temperatureMedianE.upperBound()));
+				data.addDatumDirectly(category, temperatureMedianE.toString(), new GeoDatum(date.atStartOfDay(),
+						temperatureMedian, temperatureMedianE.lowerBound(), temperatureMedianE.upperBound()));
 			if (temperatureMin != null)
-				data.addDatumDirectly(category, temperatureMinE.toString(), 
-					new GeoDatum(date.atStartOfDay(), temperatureMin, temperatureMinE.lowerBound(), temperatureMinE.upperBound()));
+				data.addDatumDirectly(category, temperatureMinE.toString(), new GeoDatum(date.atStartOfDay(),
+						temperatureMin, temperatureMinE.lowerBound(), temperatureMinE.upperBound()));
 			if (temperatureMax != null)
-				data.addDatumDirectly(category, temperatureMaxE.toString(), 
-						new GeoDatum(date.atStartOfDay(), temperatureMax, temperatureMaxE.lowerBound(), temperatureMaxE.upperBound()));
+				data.addDatumDirectly(category, temperatureMaxE.toString(), new GeoDatum(date.atStartOfDay(),
+						temperatureMax, temperatureMaxE.lowerBound(), temperatureMaxE.upperBound()));
 			if (druck != null)
 				data.addDatumDirectly(category, pressureE.toString(),
-					new GeoDatum(date.atStartOfDay(), druck, pressureE.lowerBound(), pressureE.upperBound()));
+						new GeoDatum(date.atStartOfDay(), druck, pressureE.lowerBound(), pressureE.upperBound()));
 			if (dampf != null)
 				data.addDatumDirectly(category, vaporE.toString(),
-					new GeoDatum(date.atStartOfDay(), dampf, vaporE.lowerBound(), vaporE.upperBound()));
+						new GeoDatum(date.atStartOfDay(), dampf, vaporE.lowerBound(), vaporE.upperBound()));
 			if (rel != null)
 				data.addDatumDirectly(category, humidityE.toString(),
-					new GeoDatum(date.atStartOfDay(), rel, humidityE.lowerBound(), humidityE.upperBound()));
+						new GeoDatum(date.atStartOfDay(), rel, humidityE.lowerBound(), humidityE.upperBound()));
 		}
 
 	}
 
-	public String JSONQuery(LocalDate startDate, LocalDate endDate, Point2D.Double coordinates) {
+	// extract data from the JSONObject retrieved from
+	// https://dataset.api.hub.zamg.ac.at
+	private ArrayList<Float> extractData(JSONObject jso) {
+		ArrayList<Float> data = new ArrayList<Float>();
+
+		String dataString = jso.get("data").toString();
+
+		dataString = dataString.substring(1, dataString.length() - 1);
+
+		for (String s : dataString.split(","))
+			if (s.equals("null"))
+				data.add(null);
+			else
+				data.add(Float.parseFloat(s));
+
+		return data;
+	}
+
+	// make query to https://dataset.api.hub.geosphere.at for date range with
+	// regards to coordinates and return JSON-String
+	private String JSONQuery(LocalDate startDate, LocalDate endDate, Point2D.Double coordinates) {
 
 		String jsonString = null;
-		
-		try {
-			
-			jsonString = JSONQuery(PREFIX_AUSTRIA + startDate + INFIX1_AUSTRIA + endDate + INFIX2_AUSTRIA + getNearestStationAustria(coordinates));
-			return jsonString;
-		} finally {
-			if (jsonString != null)
-				System.out.println("Got data for " + startDate + " to " + endDate);
 
-		}
+		jsonString = JSONQuery(PREFIX_AUSTRIA + startDate + INFIX1_AUSTRIA + endDate + INFIX2_AUSTRIA
+				+ getNearestStationAustria(coordinates));
+		return jsonString;
 	}
-	
-	public String JSONQuery(String query) {
+
+	// make an arbitrary JSONQuery and return JSON-String
+	private String JSONQuery(String query) {
 
 		String jsonString = null;
 
 		System.out.println("Fetching data...");
 
+		// open connection and build string
 		try {
 			URL url = new URL(query);
 			HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
@@ -171,6 +185,7 @@ public class APIQueryGeoSphereAustria implements APIQuery {
 
 			if (jsonString.isEmpty()) {
 				System.out.println("Got no data");
+				// TODO: how to handle not getting data?
 				System.exit(0);
 			}
 			br.close();
@@ -178,47 +193,57 @@ public class APIQueryGeoSphereAustria implements APIQuery {
 		} catch (MalformedURLException e) {
 			System.out.println("Something wrong with the URL.");
 			e.printStackTrace();
+			// TODO: how to handle wrong URL?
+			System.exit(0);
+
 		} catch (IOException e) {
 			System.out.println("Something wrong with the connection.");
 			e.printStackTrace();
+			// TODO: how to handle IOException?
+			System.exit(0);
 
 		}
 
 		return jsonString;
 	}
 
+	// returns the GeoSphere Austria station nearest to coordinates
 	private String getNearestStationAustria(Point2D.Double coordinates) {
-		
+
+		// TODO: get stations as objects, sort and implement Voronoi-Diagram via
+		// Fortune's algorithm: https://en.wikipedia.org/wiki/Fortune's_algorithm
+		// TODO: save stations as data structure or query every time? How often to
+		// update?
+
 		if (coordinates == null)
 			return null;
-		
+
 		Double offset = Double.POSITIVE_INFINITY;
-		
-		String jsonString = JSONQuery("https://dataset.api.hub.geosphere.at/v1/station/historical/klima-v1-1d/metadata");
-		JSONObject jsonObject = new  JSONObject(jsonString);
-		
-		
+
+		String jsonString = JSONQuery(
+				"https://dataset.api.hub.geosphere.at/v1/station/historical/klima-v1-1d/metadata");
+		JSONObject jsonObject = new JSONObject(jsonString);
+
 		JSONArray stations = jsonObject.getJSONArray("stations");
-		
+
 		String nearestStation = null;
-		
+
 		for (Object jo : stations) {
-			
+
 			jsonObject = (JSONObject) jo;
-			
+			System.out.println(jo);
 			Double lat = Double.parseDouble(jsonObject.get("lat").toString());
 			Double lon = Double.parseDouble(jsonObject.get("lon").toString());
 			Double newOffset = Math.abs((coordinates.x - lat)) + Math.abs((coordinates.y - lon));
-			
+
 			if (newOffset < offset) {
 				nearestStation = jsonObject.get("id").toString();
 				offset = newOffset;
 			}
 		}
-			
-		return nearestStation;
-	}
 
-	
+		return "105";
+//		return nearestStation;
+	}
 
 }
