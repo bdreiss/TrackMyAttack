6fa1a996b56a10d2a05959de325f9ddf18cb3334
bdreiss <bd_reiss@yahoo.de>
2024-01-12 20:05:26 +0100

    Added documentation

--- DataAPI/src/com/bdreiss/dataAPI/enums/Category.java
+++ DataAPI/src/com/bdreiss/dataAPI/enums/Category.java
@@ -6,19 +6,19 @@ package com.bdreiss.dataAPI.enums;
 
 public enum Category {
 	/**
-	 * 
+	 * Data representing ailemnts. //TODO add attributes
 	 */
 	AILMENT, 
 	/**
-	 * 
+	 * Data representing causes. //TODO add attributes
 	 */
 	CAUSE, 
 	/**
-	 * 
+	 * Data representing symptoms. //TODO add attributes
 	 */
 	SYMPTOM, 
 	/**
-	 * 
+	 * Data representing remedies. //TODO add attributes
 	 */
 	REMEDY
 }
