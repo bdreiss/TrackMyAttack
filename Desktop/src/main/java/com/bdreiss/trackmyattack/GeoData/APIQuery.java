49dfdae22a283e312276b6eef8c34de5d4d4a2d1
bdreiss <bd_reiss@yahoo.de>
2024-03-23 12:03:21 +0000

    Cleaned up a little

--- Desktop/src/main/java/com/bdreiss/trackmyattack/GeoData/APIQuery.java
+++ Desktop/src/main/java/com/bdreiss/trackmyattack/GeoData/APIQuery.java
@@ -2,7 +2,6 @@ package main.java.com.bdreiss.trackmyattack.GeoData;
 
 import java.time.LocalDate;
 
-import com.bdreiss.dataAPI.core.DataModel;
 import com.bdreiss.dataAPI.enums.Category;
 
 /**
@@ -16,7 +15,7 @@ public interface APIQuery {
 	 * 
 	 * @param startDate first date in range for which to look up data
 	 * @param endDate last date in range for which to look up data
-	 * @param GeoData AbstractDataModel in which data should be added 
+	 * @param geoData AbstractDataModel in which data should be added 
 	 * @param category Category that is used for storing data
 	 */
 	void query(LocalDate startDate, LocalDate endDate, GeoData geoData, Category category);
